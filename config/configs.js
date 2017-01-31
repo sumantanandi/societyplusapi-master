28dffe5d9468e174b784dcc852c622b37f5d2bd8
DIR\sumanta.nandi <sumanta.nandi@accenture.com>
2017-02-01 00:59:25 +1100

    initial chnage

---
+++ config/configs.js
@@ -0,0 +1,42 @@
+
+{
+  "local": {
+    "label": "local",
+      "apiKeys": {
+      "username": "",
+        "password" :""
+    },
+    "http": {
+      "address": "localhost",
+        "port": 8080
+    },
+    "db": {
+      "connectString": "postgres://ythaatkhclsrbf:dpb30mSasjoou_bWUS2WeKjt0q@ec2-54-225-195-254.compute-1.amazonaws.com:5432/delv202g5ov939"
+    },
+    "messages": {
+      "quotaguardstaticURL": "",
+        "notificationURL": "",
+          "applicationResourceURL":"",
+            "client_id":"",
+              "client_secret":"",
+                "grant_type":"",
+                  "username":"",
+                    "password":"",
+                      "scope":"api"
+    },
+    "salesforce": {
+      "loginUrl": "https://test.salesforce.com",
+        "clientId": "3MVG9Se4BnchkASkLWv2TnL4oThIYCj1_J_Aa.XaH1UEzb5niJ.mEPeJjArrURfXdG6wZgGhkEPs4Q6ZUq_JM",
+          "clientSecret": "7765373667982255622",
+            "redirectUri": "http://localhost:3000/oauth/_callback",
+              "apiVersion": "v35.0",
+                "environment":"sandbox",
+                  "username":"sumanta.nandi@accenture.com.newco.sit",
+                    "password":"S1%ummer",
+                      "mode":"multi",
+                        "Branch__c":"a0A9000000NjWJk",
+                          "Brand_Lookup__c":"a0f90000003ZwGj",
+                            "Product_Id__c":"a0w90000002EplC"
+    }
+  }
+}
